9e90a9c172bf6be18119b08e76ebb67c2257c65f
caahkaori <carolinetakano8@gmail.com>
2020-06-17 17:59:27 +0900

    First Commit

---
+++ calculator.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+
+  res.sendFile(__dirname + "/index.html");
+
+});
+
+app.post("/", function(req, res){
+
+
+    var num1 = Number(req.body.n1);
+    var num2 = Number(req.body.n2);
+
+    var result = num1 + num2;
+
+    res.send("The result is " + result);
+
+});
+
+
+app.get("/bmicalculator", function(req, res){
+
+  res.sendFile(__dirname + "/bmiCalculator.html");
+
+});
+
+app.post("/bmicalculator", function(req, res){
+
+
+    var weight = Number(req.body.wei);
+    var height = Number(req.body.hei);
+
+    var total = weight + height;
+
+    res.send("The result is " + total);
+
+});
+
+
+app.listen(4000);
